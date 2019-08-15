89fea2820370106a741f8a4517725994c9cd3565
Sara McCombs <thesaramccombs@gmail.com>
2019-08-15 19:39:35 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,7 +3,7 @@
 # method outputs "#{number} SECOND(S)!" on each iteration
 # method returns "HAPPY NEW YEAR!"
 
-def countdown(integer_start_number)
+def countdown (integer_start_number)
   phrase = "HAPPY NEW YEAR!"
   
   while integer_start_number < 0 
